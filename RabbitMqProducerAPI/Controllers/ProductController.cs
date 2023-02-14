b776e03a41ba3e1b7f4297059c8ee9179db303a3
Peter Vais <peter-vais@hotmail.sk>
2023-02-14 20:18:54 +0100

    Code cleanup

--- RabbitMqProducerAPI/Controllers/ProductController.cs
+++ RabbitMqProducerAPI/Controllers/ProductController.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using RabbitMqProducerAPI.Models;
 using RabbitMqProducerAPI.RabbitMQ;
 using RabbitMqProducerAPI.Services;
